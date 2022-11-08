93d01a21c58e7a5ad3b0887cc55b89fdef16e1e4
Orangesmw <106233500+Orangesmw1@users.noreply.github.com>
2022-11-09 00:42:57 +0700

    done stop & run carousel

--- js/carousel-stop.js
+++ js/carousel-stop.js
@@ -1,20 +1,36 @@
-var readContent = document.querySelector(".read-content");
-var active = document.querySelector(".carousel-inner.active");
-var carouselItem = document.querySelector(".carousel-item")
 
+var carouselItem1 = document.querySelector("#carousel-item-1");
+var carouselItem2 = document.querySelector("#carousel-item-2");
+var carouselItem3 = document.querySelector("#carousel-item-3");
 
 function stopCarousel() {
-    active.style = `
-    display: block;
-    `
-    carouselItem.style = `
-    display: none
-    `
+    
+        if(carouselItem1.className == "carousel-item active") {
+            carouselItem1.classList.remove("active");
+            carouselItem1.classList.add("block-stop-carousel");
+
+        } else if(carouselItem2.className == "carousel-item active") {
+            carouselItem2.classList.remove("active");
+            carouselItem2.classList.add("block-stop-carousel");
+
+        } else {
+            carouselItem3.classList.remove("active");
+            carouselItem3.classList.add("block-stop-carousel");
+        }   
 }
 
+
 function runCarousel() {
-    active.style = `
-    display: block ;
-    `
-    
+    if(carouselItem1.className == "carousel-item block-stop-carousel") {
+        carouselItem1.classList.add("active");
+        carouselItem1.classList.remove("block-stop-carousel");
+
+    } else if(carouselItem2.className == "carousel-item block-stop-carousel") {
+        carouselItem2.classList.add("active");
+        carouselItem2.classList.remove("block-stop-carousel");
+    } else {
+        carouselItem3.classList.add("active");
+        carouselItem3.classList.remove("block-stop-carousel");
+
+    }   
 }
